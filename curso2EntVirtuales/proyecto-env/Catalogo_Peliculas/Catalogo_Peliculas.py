41d1cb8ebfa1d39c243e214751b1905bf0f191e6
Emerson Escalante <eam.escalante@gmail.com>
2024-02-18 21:12:33 -0600

    Agregando ejecicio pipenv

---
+++ curso2EntVirtuales/proyecto-env/Catalogo_Peliculas/Catalogo_Peliculas.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main():
+    ventana = tk.Tk()
+    ventana.title("Catalogo de peliculas")
+    ventana.iconbitmap("image/logo.ico")
+    ventana.mainloop()
+
+if __name__ == '__main__':
+    main()
